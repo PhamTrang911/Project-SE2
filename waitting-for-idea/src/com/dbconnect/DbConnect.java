4827ad211733b09af78027d6ce8c5f5e493f8f1a
PhamTrang911 <phamtrangxt19@gmail.com>
2021-05-04 22:08:20 +0700

    change database connection to fit with new database hosting

--- waitting-for-idea/src/com/dbconnect/DbConnect.java
+++ waitting-for-idea/src/com/dbconnect/DbConnect.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DbConnect {
-    private static String DB_URL = "jdbc:mysql://mysql-27918-0.cloudclusters.net:27918/se2_database?useSSL=false";
+    private static String DB_URL = "jdbc:mysql://mysql-30093-0.cloudclusters.net:30093/se2_database?useSSL=false";
     private static String USER_NAME = "admin";
-    private static String PASSWORD = "laleafhanu";
+    private static String PASSWORD = "trang1999";
     public static Connection getConnection()  {
 		Connection conn = null;
 		try {
